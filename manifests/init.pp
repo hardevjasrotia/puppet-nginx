1709ac00fe5c22b9513c740bbe9438e66c312704
Your Name <you@example.com>
2018-04-05 08:11:27 +0000

    Add module skelenton

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,48 +1,27 @@
-# Class: nginx
-# ===========================
-#
-# Full description of class nginx here.
-#
-# Parameters
-# ----------
-#
-# Document parameters here.
-#
-# * `sample parameter`
-# Explanation of what this parameter affects and what it defaults to.
-# e.g. "Specify one or more upstream ntp servers as an array."
-#
-# Variables
-# ----------
-#
-# Here you should define a list of variables that this module would require.
-#
-# * `sample variable`
-#  Explanation of how this variable affects the function of this class and if
-#  it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#  External Node Classifier as a comma separated list of hostnames." (Note,
-#  global variables should be avoided in favor of class parameters as
-#  of Puppet 2.6.)
-#
-# Examples
-# --------
-#
-# @example
-#    class { 'nginx':
-#      servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#    }
-#
-# Authors
-# -------
-#
-# Author Name <author@domain.com>
-#
-# Copyright
-# ---------
-#
-# Copyright 2018 Your name here, unless otherwise noted.
-#
-class nginx {
+class nginx (
+  String $ensure		        = $::nginx::params::ensure,
+  String $package_name		        = $::ngnix::params::package_name,
+  String $config_dir		        = $::ngnix::params::config_dir,
+  String $config_ensure		        = $::ngnix::params::config_ensure,
+  String $config_mode		        = $::ngnix::params::config_mode,
+  String $config_owner		        = $::nginx::params::config_owner,
+  String $config_group		        = $::nginx::params::config_group,
+  String $config_confd	 	        = $::ngnix::params::config_confd,
+  String $config_log_dir	        = $::ngnix::params::config_log_dir,
+  String $config_pid_file	        = $::ngnix::params::config_pid_file,
+  Optional[String] $config_vdir_enable  = $::ngnix::params::config_vdir_enable,
+  String $config_process_user		= $::ngnix::params::config_process_user, 
+  String $service_pattern		= $::ngnix::params::service_pattern,
+  String $service_ensure		= $::ngnix::params::service_ensure,
+  String $service_name			= $::ngnix::params::service_name, 
+  Boolean $service_enable		= $::ngnix::params::service_enable,
+  Boolean $service_hasstatus		= $::ngnix::params::service_hasstatus,
+  Boolean $service_hasrestart		= $::ngnix::parans::service_hasrestart,
+  String $docroot			= $::ngnix::params::docroot,
 
+) inherits ::ngnix::params {
+  class {'::ngnix::install':}
+  -> class {'::ngnix::config':}
+  -> class {'::ngnix::service':}
 
 }
